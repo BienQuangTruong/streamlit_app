d2972698a8090c3389b9763b1a9705d3181661dd
BienQuangTruong <bqtruong0104@gmail.com>
2020-12-31 15:57:07 +0700

    first commit

---
+++ awesome_streamlit/database/tags.py
@@ -0,0 +1,37 @@
+"Module containing Tags"
+from awesome_streamlit.shared.models import Tag
+
+# Tags
+ALTERNATIVE = Tag(name="Alternative")
+APP = Tag("App")
+APP_IN_GALLERY = Tag(name="App In Gallery")
+ARTICLE = Tag(name="Article")
+AWESOME_STREAMLIT_ORG = Tag(name="Awesome-Streamlit.org")
+AWESOMESTREAMLIT_COMPONENT_AUTHOR = Tag(name="The beginning")
+CODE = Tag("Code")
+DEPLOYMENT = Tag(name="Deployment")
+FINANCE = Tag(name="Finance")
+GUIDE = Tag(name="Guide")
+IMAGE_RECOGNITION = Tag(name="Image Recognition")
+MACHINE_LEARNING = Tag(name="Machine Learning")
+NLP = Tag(name="NLP")
+SISTER_SITES = Tag(name="Sister Sites")
+SOCIAL = Tag(name="Social")
+STREAMLIT_TAG = Tag(name="Streamlit.io")
+STREAMLIT_COMPONENT = Tag(name="Component")
+STREAMLIT_EXAMPLE = Tag(name="Example")
+TECHNICAL = Tag(name="Technical")
+TUTORIAL = Tag(name="Tutorial")
+VIDEO = Tag(name="Video")
+VOILA = Tag(name="Voila")
+KNN = Tag(name="K-Nearest Neighbors")
+MLP = Tag(name="Multi-layer Perceptron")
+PLS = Tag(name="partialLeastSquares")
+SVR = Tag(name="SVM Regression")
+
+TAGS = [
+    KNN,
+    MLP,
+    PLS,
+    SVR,
+]
