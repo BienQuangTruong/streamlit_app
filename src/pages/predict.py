9bde5ee7ec3149820a31de055843242309782e69
phuocnv3008 <phuocnv3008@gmail.com>
2021-01-15 02:00:15 +0700

    2:00-15/1/2021

--- src/pages/predict.py
+++ src/pages/predict.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
+
 def write():
 
     st.title(" DỰ ĐOÁN (PREDICT) ")
@@ -14,7 +15,7 @@ def write():
     uploaded_file = st.file_uploader("Chọn tập dữ liệu CSV từ máy của bạn")
 
     def download_link(object_to_download, download_filename, download_link_text):
-        if isinstance(object_to_download,pd.DataFrame):
+        if isinstance(object_to_download, pd.DataFrame):
             object_to_download = object_to_download.to_csv(index=False)
 
         # some strings <-> bytes conversions necessary here
@@ -22,36 +23,33 @@ def write():
 
         return f'<a href="data:file/txt;base64,{b64}" download="{download_filename}">{download_link_text}</a>'
 
-    if st.button('Run predict'):
-        if uploaded_file is not None and uploaded_model is not None:
-            df = pd.read_csv(uploaded_file, low_memory=False)
-            model = pickle.load(uploaded_model)
-            
-            # Normalize
-            sc = StandardScaler()
-            x_test = sc.fit_transform(df)
+    if uploaded_file is not None and uploaded_model is not None:
+        df = pd.read_csv(uploaded_file, low_memory=False)
+        model = pickle.load(uploaded_model)
 
-            #load model
-            # clf = pickle.load(open('classifier_models.pkl', 'rb'))
+        # Normalize
+        sc = StandardScaler()
+        x_test = sc.fit_transform(df)
 
-            pred_clf = model.predict(x_test)
+        # load model
+        # clf = pickle.load(open('classifier_models.pkl', 'rb'))
 
-            bad = []
-            good = []
-            for i in pred_clf:
-                if i == 0:
-                    bad.append(i)
-                else:
-                    good.append(i)
+        pred_clf = model.predict(x_test)
 
-            df['tasty'] = ['Bad' if x == 0 else 'Good' for x in pred_clf]
+        bad = []
+        good = []
+        for i in pred_clf:
+            if i == 0:
+                bad.append(i)
+            else:
+                good.append(i)
 
-            st.write('Đã đánh giá tự động xong !!!', df)
-            st.write('Bad: ', len(bad))
-            st.write('Good: ', len(good))
-            
+        df['tasty'] = ['Bad' if x == 0 else 'Good' for x in pred_clf]
 
+        st.write('Đã đánh giá tự động xong !!!', df)
+        st.write('Bad: ', len(bad))
+        st.write('Good: ', len(good))
 
-            if st.button('Download Dataframe as CSV'):
-                tmp_download_link = download_link(df, 'predict.csv', 'Click here to download your data!')
-                st.markdown(tmp_download_link, unsafe_allow_html=True)
+        if st.button('Download Dataframe as CSV'):
+            tmp_download_link = download_link(df, 'predict.csv', 'Click here to download your data!')
+            st.markdown(tmp_download_link, unsafe_allow_html=True)
